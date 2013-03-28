eead4d85106794a576a313df7e28f52c56da97ae
Nathan Yam <nathancyam@gmail.com>
2013-03-28 12:00:30 +1100

    Added a lot of stuff

--- pagenav.js
+++ pagenav.js
@@ -3,9 +3,13 @@
  */
 $(document).ready(function(){
 	$('#linkContacts').click(goContacts());
+	$('#linkInfo').click(goInfo());
 	
 	$(function goContacts(){
 		$("#main").load("contacts.html");		
+	});	
+	$(function goInfo(){
+		$("#main").load("info.html");		
 	});	
 	
 	$("#banner").click(function(){
